e3f4bb25d55753ecc5084c65c4de2bece62482fa
inderby <youk8253@gmail.com>
2024-10-14 17:13:34 +0900

    #14 - 클래스 Attribute 및 생성자 정의
    추가로 Closeable을 상속해놨음.

--- src/main/java/org/atg/realwearappbackend/room/Room.java
+++ src/main/java/org/atg/realwearappbackend/room/Room.java
@@ -1,4 +1,37 @@
 package org.atg.realwearappbackend.room;
 
-public class Room {
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.atg.realwearappbackend.user.UserSession;
+import org.kurento.client.MediaPipeline;
+
+import javax.annotation.PreDestroy;
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Slf4j
+public class Room implements Closeable {
+    private final ConcurrentHashMap<String, UserSession> participants = new ConcurrentHashMap<>();
+    private final MediaPipeline pipeline;
+    @Getter
+    private final String roomName;
+
+    public Room(String roomName, MediaPipeline pipeline) {
+        this.roomName = roomName;
+        this.pipeline = pipeline;
+
+        log.info("ROOM {} has been created", roomName);
+    }
+
+    @PreDestroy
+    private void shutdown() {
+        this.close();
+    }
+
+    @Override
+    public void close() {
+
+    }
 }
