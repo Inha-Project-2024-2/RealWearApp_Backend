92acca04dd1a1011914fa8fb610b76637400b607
inderby <youk8253@gmail.com>
2024-10-14 17:23:42 +0900

    #14 - join 함수 구현
    사용자가 방에 참가했을 때 호출할 함수를 구현했다.
    내부 호출 함수는 추가로 구현해야 됨.

--- src/main/java/org/atg/realwearappbackend/room/Room.java
+++ src/main/java/org/atg/realwearappbackend/room/Room.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.atg.realwearappbackend.user.UserSession;
 import org.kurento.client.MediaPipeline;
+import org.springframework.web.socket.WebSocketSession;
 
 import javax.annotation.PreDestroy;
 import java.io.Closeable;
@@ -22,12 +23,29 @@ public class Room implements Closeable {
         this.roomName = roomName;
         this.pipeline = pipeline;
 
-        log.info("ROOM {} has been created", roomName);
+        log.info("방 {} 생성", roomName);
     }
 
     @PreDestroy
     private void shutdown() {
         this.close();
+    }
+
+    public UserSession join(String username, WebSocketSession session) {
+        log.info("방 {} : 참가자 {} 추가", this.roomName, username);
+        final UserSession participant = new UserSession(username, roomName, this.pipeline, session);
+
+        joinRoom(participant);
+        participants.put(username, participant);
+        sendParticipantNames(participant);
+        return participant;
+    }
+
+    private void joinRoom(UserSession participant) {
+
+    }
+
+    private void sendParticipantNames(UserSession participant) {
     }
 
     @Override
