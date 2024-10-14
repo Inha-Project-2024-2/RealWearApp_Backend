b63d1608375031c4d1b42db320fc5b735f7ad47b
inderby <youk8253@gmail.com>
2024-10-14 15:14:22 +0900

    #10 - 세션의 attribute와 생성자 정의
    세션이 갖고 있는 속성은 아래와 같다.
    - 사용자 이름
    - WebSocket 세션
    - Kurento에서 제공해준 MediaPipeline
    - 참가한 방 번호
    - 비디오 데이터를 밖으로 보낼 WebRtcEndpoint
    - 비디오 데이터들을 받을 WebRtcEndpoints
    
    생성자에서 유의할 점은 outboundEndpoint에 리스너를 등록해줘야 한다는 것이다.
    로컬 피어(outboundEndpoint) 에서 새로운 ICE 후보가 발견될 때마다 실행되는 리스너를 등록해야 NAT Traversal을 할 수 있다.

--- src/main/java/org/atg/realwearappbackend/user/UserSession.java
+++ src/main/java/org/atg/realwearappbackend/user/UserSession.java
@@ -1,4 +1,49 @@
 package org.atg.realwearappbackend.user;
 
+import com.google.gson.JsonObject;
+import lombok.extern.slf4j.Slf4j;
+import org.kurento.client.EventListener;
+import org.kurento.client.IceCandidateFoundEvent;
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.WebRtcEndpoint;
+import org.kurento.jsonrpc.JsonUtils;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
 public class UserSession {
+    private final String name;
+    private final WebSocketSession session;
+
+    private final MediaPipeline pipeline;
+
+    private final String roomName;
+
+    private final WebRtcEndpoint outboundEndpoint;
+    private final ConcurrentHashMap<String, UserSession> inboundEndPoint = new ConcurrentHashMap<>();
+
+    public UserSession(String name, WebSocketSession session, MediaPipeline pipeline, String roomName, WebRtcEndpoint outboundEndpoint) {
+        this.name = name;
+        this.session = session;
+        this.pipeline = pipeline;
+        this.roomName = roomName;
+        this.outboundEndpoint = outboundEndpoint;
+
+        this.outboundEndpoint.addIceCandidateFoundListener(event -> {
+            JsonObject response = new JsonObject();
+            response.addProperty("id", "iceCandidate");
+            response.addProperty("name", name);
+            response.add("candidate", JsonUtils.toJsonObject(event.getCandidate()));
+            try {
+                synchronized (session) {
+                    session.sendMessage(new TextMessage(response.toString()));
+                }
+            } catch (IOException e) {
+                log.debug(e.getMessage());
+            }
+        });
+    }
 }
