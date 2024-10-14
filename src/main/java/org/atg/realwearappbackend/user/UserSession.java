6810f3f67bab7dc9a3b5a29574648092ae30569f
inderby <youk8253@gmail.com>
2024-10-14 15:45:03 +0900

    #10 - 다른 참가자의 outbound 미디어 스트림을 현재 사용자의 inbound에 추가
    자기 자신의 화면을 보기 위한 loopback을 설정하고,
    다른 사용자의 outbound를 inbound로 연결해준다.

--- src/main/java/org/atg/realwearappbackend/user/UserSession.java
+++ src/main/java/org/atg/realwearappbackend/user/UserSession.java
@@ -52,4 +52,37 @@ public class UserSession {
             }
         });
     }
+    private WebRtcEndpoint getEndpointForUser(final UserSession sender){
+        if(sender.getName().equals(name)){
+            log.debug("참가자 {} : loopback", this.name); // 자기 자신의 화면을 보기 위한 설정 및 추후 필터 적용을 위한 도구
+            return outboundEndpoint;
+        }
+
+        log.debug("참가자 {} : 참가자 {}의 비디오를 받는 중", this.name, sender.getName());
+
+        WebRtcEndpoint inbound = inboundEndPoint.get(sender.getName());
+        if(inbound == null){
+            log.debug("참가자 {} : 기존의 엔드포인트가 없기 때문에 참가자 {} 를 위한 새로운 엔드 포인트 생성", this.name, sender.getName());
+            inbound = new WebRtcEndpoint.Builder(pipeline).build();
+
+            inbound.addIceCandidateFoundListener(event -> {
+                JsonObject response = new JsonObject();
+                response.addProperty("id", "iceCandidate");
+                response.addProperty("name", sender.getName());
+                response.add("candidate", JsonUtils.toJsonObject(event.getCandidate()));
+                try {
+                    synchronized (session) {
+                        session.sendMessage(new TextMessage(response.toString()));
+                    }
+                } catch (IOException e) {
+                    log.debug(e.getMessage());
+                }
+            });
+            inboundEndPoint.put(sender.getName(), inbound);
+        }
+        log.debug("참가자 {} : 참가자 {} 의 엔드 포인트 생성", this.name, sender.getName());
+
+        sender.getOutboundEndpoint().connect(inbound);
+        return inbound;
+    }
 }
